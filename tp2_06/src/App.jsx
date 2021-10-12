b98e5dc0a1a6c16ea878ba80111382b8b9e71ce7
MaryseThomassin <86240358+MaryseThomassin@users.noreply.github.com>
2021-10-12 12:15:02 -0400

    Début tp2
    Placer la base

---
+++ tp2_06/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Routes from './routes';
+
+const App = () => (
+  <>
+    <Header />
+    <Routes />
+    <Footer />
+  </>
+);
+
+export default App;
