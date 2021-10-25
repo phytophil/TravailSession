55343cadeadba4eea2b46eee0eb28446e79aff87
Syl-Dub <sylviedubedev@gmail.com>
2021-10-25 14:18:28 -0400

    tried min css
    plusieurs essais mais ne semble pas fonctionner

--- TP1_05/gulpfile.js
+++ TP1_05/gulpfile.js
@@ -1,5 +1,19 @@
+
 var gulp = require('gulp');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('hello', function(){
     console.log('Hello Zell');
 });
+
+gulp.task('useref', function(){
+    return gulp.src('/TP1_05/*.html')
+      .pipe(useref())
+      //.pipe(gulpIf('*.js', uglify()))
+      // Minifies only if it's a CSS file
+      .pipe(gulpIf('/TP1_05/*.css', cssnano()))
+      .pipe(gulp.dest('/TP1_05/dist'))
+  });
