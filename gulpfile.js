4afeaa039ee9a1d9828951f0911aaae1cd78db11
deniserodriguessilva <denisesilva.pro@gmail.com>
2021-10-26 21:22:05 -0400

    gulp
    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+  }
+  
+  exports.default = defaultTask
+
+  const gulp = require('gulp');
+
+const { src, dest, parallel } = require('gulp');
+
+const uglify = require('gulp-uglify');
+
+const rename = require('gulp-rename');
+
+const cssnano = require('gulp-cssnano');
+
+
+
+function js() {
+
+return src('app/js/*.js')
+
+.pipe(uglify())
+
+.pipe(rename({ extname: '.min.js' }))
+
+.pipe(dest('dist/js/'));
+
+}
+
+
+
+function css() {
+
+return src('app/css/*.css')
+
+.pipe(cssnano())
+
+.pipe(rename({ extname: '.min.css' }))
+
+.pipe(dest('dist/css/'));
+
+}
+
+
+
+function fonts() {
+
+return src('app/fonts/**/*')
+
+.pipe(dest('dist/fonts/'));
+
+}
+
+
+
+// exports.default = parallel(js, css, fonts);
+
+exports.css = css;
