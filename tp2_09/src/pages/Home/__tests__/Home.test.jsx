9c528da6e8ea943e6894fb1d5bbbbf74c0998870
phytophil <86239708+phytophil@users.noreply.github.com>
2021-10-28 22:48:32 -0400

    restore stash complété

---
+++ tp2_09/src/pages/Home/__tests__/Home.test.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import Home from '..';
+
+describe('Home', () => {
+  it('renders the title and description', () => {
+    render(
+      <Router basename="/">
+        <Switch>
+          <Home />
+        </Switch>
+      </Router>
+    );
+
+    expect(screen.getByText('Welcome to your new React App!')).toBeInTheDocument();
+    expect(screen.getByText('Made with ❤️')).toBeInTheDocument();
+  });
+});
