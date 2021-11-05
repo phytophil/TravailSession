10ccdb2304cb8c8cf6907e9a0fa83bdaa21538ef
MaryseThomassin <86240358+MaryseThomassin@users.noreply.github.com>
2021-11-04 21:29:22 -0400

    Tentative d'ajout antd
    Tentative avec problème chemin à vérifier pour husky et git

---
+++ tp2_06/react_demo/my-app/src/lib/Datepicker/Datepicker.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './Datepicker.module.css';
+
+const Datepicker = ({initialValue = 0}) => {
+
+
+    const {count, incrementCount} = useLogic(initialValue);
+  
+    return (
+      <div className={classes.test}>
+       
+      </div>
+    );
+  };
+  
+Datepicker.propTypes = {
+    /**
+     * Define initial value for the Datepicker
+     */
+    initialValue: PropTypes.string.isRequired,
+  };
+  
+  export default Datepicker;
