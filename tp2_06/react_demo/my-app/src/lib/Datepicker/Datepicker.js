371e9280c73ffca0eadd115cd1a704dd02ffd97c
MaryseThomassin <86240358+MaryseThomassin@users.noreply.github.com>
2021-11-08 14:41:05 -0500

    Essai react suite
    Encore plusieurs erreurs à vérifier...

--- tp2_06/react_demo/my-app/src/lib/Datepicker/Datepicker.js
+++ tp2_06/react_demo/my-app/src/lib/Datepicker/Datepicker.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classes from './Datepicker.module.css';
+import {DatePicker, Space} from 'antd';
 
-const Datepicker = ({initialValue = 0}) => {
+const {RangePicker} = DatePicker;
 
+function onChange(value, dateString) {
+  console.log('Selected Time: ', value);
+  console.log('Formatted Selected Time: ', dateString);
+}
 
-    const {count, incrementCount} = useLogic(initialValue);
-  
-    return (
-      <div className={classes.test}>
-       
-      </div>
-    );
-  };
-  
+function onOk(value) {
+  console.log('onOk: ', value);
+}
+
+const Datepicker = ({picker}) => (
+  <Space direction="vertical" size={12}>
+    <DatePicker showTime onChange={onChange} onOk={onOk} picker={picker} />
+    <RangePicker
+      showTime={{format: 'HH:mm'}}
+      format="YYYY-MM-DD HH:mm"
+      onChange={onChange}
+      onOk={onOk}
+    />
+  </Space>
+);
+
 Datepicker.propTypes = {
-    /**
-     * Define initial value for the Datepicker
-     */
-    initialValue: PropTypes.string.isRequired,
-  };
-  
-  export default Datepicker;
+  /**
+   * Define initial value for the Datepicker picker (week,month,quarter,year)
+   */
+  picker: PropTypes.string.isRequired,
+};
+
+export default Datepicker;
