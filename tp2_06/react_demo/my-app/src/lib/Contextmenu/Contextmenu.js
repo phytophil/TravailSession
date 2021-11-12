fe4dc25ae106006f241273edb9e1fc64ad41d9ec
MaryseThomassin <86240358+MaryseThomassin@users.noreply.github.com>
2021-11-11 19:44:08 -0500

    Tentative d'ajout de composante Blueprint
    Tentative d'ajout de composant Blueprint Contextmenu

---
+++ tp2_06/react_demo/my-app/src/lib/Contextmenu/Contextmenu.js
@@ -0,0 +1,17 @@
+import {Menu, MenuItem} from '@blueprintjs/core';
+import {ContextMenu2} from '@blueprintjs/popover2';
+
+export default function ContextMenuExample() {
+  return (
+    <ContextMenu2
+      content={
+        <Menu>
+          <MenuItem text="Save" />
+          <MenuItem text="Save as..." />
+          <MenuItem text="Delete..." intent="danger" />
+        </Menu>
+      }>
+      <div className="my-context-menu-target">Right click me!</div>
+    </ContextMenu2>
+  );
+}
