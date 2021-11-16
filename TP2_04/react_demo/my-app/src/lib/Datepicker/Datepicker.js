2afb7d7277d45c58e65f4e135c489bfeadb8b768
salutmarcus <marcuslima@outlook.com>
2021-11-16 12:10:16 -0500

    bug storybook

--- TP2_04/react_demo/my-app/src/lib/Datepicker/Datepicker.js
+++ TP2_04/react_demo/my-app/src/lib/Datepicker/Datepicker.js
@@ -6,33 +6,31 @@ import {DatePicker, Space} from 'antd';
 const {RangePicker} = DatePicker;
 
 function onChange(value, dateString) {
-    console.log('Selected Time: ', value);
-    console.log('Formatted Selected Time: ', dateString);
+  console.log('Selected Time: ', value);
+  console.log('Formatted Selected Time: ', dateString);
 }
 
 function onOk(value) {
-    console.log('onOk: ', value);
+  console.log('onOk: ', value);
 }
 
-const Datepicker = ({picker}) => {
-    return (
-        <Space direction="vertical" size={12}>
-            <DatePicker showTime onChange={onChange} onOk={onOk} picker={picker} />
-            <RangePicker
-                showTime={{format: 'HH:mm'}}
-                format="YYYY-MM-DD HH:mm"
-                onChange={onChange}
-                onOk={onOk}
-            />
-        </Space>
-    );
-};
+const Datepicker = ({picker}) => (
+  <Space direction="vertical" size={12}>
+    <DatePicker showTime onChange={onChange} onOk={onOk} picker={picker} />
+    <RangePicker
+      showTime={{format: 'HH:mm'}}
+      format="YYYY-MM-DD HH:mm"
+      onChange={onChange}
+      onOk={onOk}
+    />
+  </Space>
+);
 
 Datepicker.propTypes = {
-    /**
-     * Define initial value for the Datepicker picker (week, month, quarter,year)
-     */
-    picker: PropTypes.string.isRequired,
+  /**
+   * Define initial value for the Datepicker picker (week, month, quarter,year)
+   */
+  picker: PropTypes.string.isRequired,
 };
 
 export default Datepicker;
