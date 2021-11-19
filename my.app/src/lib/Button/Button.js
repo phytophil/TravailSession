adc041a2dfb5a310d551275206540b1e732256a1
Anaisacornejo <anaisacornejo@gmail.com>
2021-11-19 09:01:34 -0500

    composant Checkbox

--- my.app/src/lib/Button/Button.js
+++ my.app/src/lib/Button/Button.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import classes from './button.module.css';
 
 export default function ContainedButtons() {
   return (
     <Stack direction="row" spacing={2}>
       <Button variant="contained">Contained</Button>
-      <Button variant="contained" disabled>
+      <Button 
+      class={classes.ButtonAction}
+      variant="contained" disabled>
         Disabled
       </Button>
       <Button variant="contained" href="#contained-buttons">
