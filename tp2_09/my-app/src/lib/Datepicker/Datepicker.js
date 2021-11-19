22d89ccfbc11e4ba473c6763fc2e68c0b9709ed5
phytophil <vuillardetbonnard@gmail.com>
2021-11-19 11:33:02 -0500

    Update Datepicker.js
    Le changement minime à la constante du même nom que le répertoire aura permis de visualiser la composante DatePicker dans la storybook.

--- tp2_09/my-app/src/lib/Datepicker/Datepicker.js
+++ tp2_09/my-app/src/lib/Datepicker/Datepicker.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {DatePicker,Space} from 'antd';
+import {DatePicker, Space} from 'antd';
 
 const {RangePicker} = DatePicker;
 
 function onChange(value, dateString) {
-console.log('Selected Time: ', value);
-console.log('Formatted Selected Time: ', dateString);
+  console.log('Selected Time: ', value);
+  console.log('Formatted Selected Time: ', dateString);
 }
 
 function onOk(value) {
-console.log('onOk: ',value);
+  console.log('onOk: ', value);
 }
 
-const DatePicker = ({picker}) => {
-
-  return (
-    <Space direction="vertical" size={12}>
-      <DatePicker showTime onChange={onChange} onOk={onOk} picker={picker}/>
-      <RangePicker
-        showTime={{format:'HH:mm'}}
-        format="YYYY-MM-DD HH:mm"
-        onChange={onChange}
-        onOk={onOk}
-      />
-    </Space>
-  );
-};
-
+const Datepicker = ({picker}) => (
+  <Space direction="vertical" size={12}>
+    <DatePicker showTime onChange={onChange} onOk={onOk} picker={picker} />
+    <RangePicker
+      showTime={{format: 'HH:mm'}}
+      format="YYYY-MM-DD HH:mm"
+      onChange={onChange}
+      onOk={onOk}
+    />
+  </Space>
+);
 
 Datepicker.propTypes = {
   /**
